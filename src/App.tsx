2e70032285350d90870dfcd609e3d495ed326495
AymenDellas <aymen.dellas@etu.usthb.dz>
2025-07-14 22:59:35 +0100

    react import fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-// src/App.js
-import React from "react";
-
 import Hero from "./components/Hero";
 import StorySection from "./components/StorySection";
 import ComparisonSection from "./components/ComparisonSection";
